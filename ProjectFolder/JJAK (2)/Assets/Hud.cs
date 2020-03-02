feacb1306dcb5d09817ca4efe7c364618e3c18c4
aDurk47 <59945717+aDurk47@users.noreply.github.com>
2020-03-01 20:10:32 -0500

    Battle, Character, Firebase
    Additions to battle system, character creation, and google firebase
    
    Co-Authored-By: Jonathan Perez <jrp5717@psu.edu>
    Co-Authored-By: kna5125 <kna5125@users.noreply.github.com>

---
+++ ProjectFolder/JJAK (2)/Assets/Hud.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hud : MonoBehaviour
+{
+    public Text playerName;
+    public HealthBar healthBar;
+
+    public void SetHud(Unit unit)
+    {
+        playerName.text = unit.unitName;
+        healthBar.SetMaxHealth(unit.Health);
+    }
+
+    public void SetHP(int hp)
+    {
+        healthBar.SetHealth(hp);
+    }
+}
---
+++ ProjectFolder/JJAK (2)/Assets/Unit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string unitName;
+    public int Health;
+    public int curHealth;
+    public int damage;
+    public int agility;
+    public int might;
+}
